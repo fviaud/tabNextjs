1d9e2bc01a56b4634cbf067d95a2721592fdf592
Frédéric VIAUD <frederic.viaud@hotmail.fr>
2022-10-23 07:29:41 +0000

    sav

--- pages/api/auth/callback/google.js
+++ pages/api/auth/callback/google.js
@@ -1,8 +1,10 @@
 export default async function handler(req, res) {
   const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
   const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
-  const GOOGLE_REDIRECT_URI = "http://localhost:3000/api/auth/callback/google"
-  const url = "https://oauth2.googleapis.com/token"
+  const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI
+  const confGoogle = await fetch("https://accounts.google.com/.well-known/openid-configuration").then((response) =>
+    response.json()
+  )
 
   const details = {
     grant_type: "authorization_code",
@@ -21,7 +23,7 @@ export default async function handler(req, res) {
   }
 
   const oidcCredentials = `${GOOGLE_CLIENT_ID}:${GOOGLE_CLIENT_SECRET}`
-  const data = await fetch(`${url}`, {
+  const data = await fetch(confGoogle.token_endpoint, {
     method: "POST",
     headers: {
       Authorization: `Basic ${Buffer.from(oidcCredentials).toString("base64")}`,
@@ -30,7 +32,7 @@ export default async function handler(req, res) {
     body: formBody.join("&"),
   }).then((r) => r.json())
 
-  const curentUser = await fetch(`https://openidconnect.googleapis.com/v1/userinfo`, {
+  const curentUser = await fetch(confGoogle.userinfo_endpoint, {
     method: "GET",
     headers: { Authorization: `Bearer ${data.access_token}` },
   }).then((r) => r.json())
--- pages/api/auth/login/google.js
+++ pages/api/auth/login/google.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
   // const GOOGLE_CLIENT_SECRET = process.env.("GOOGLE_CLIENT_SECRET")
   const GOOGLE_REDIRECT_URI = "http://localhost:3000/api/auth/callback/google"
-  const url = `${data.authorization_endpoint}/auth?response_type=code&client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&scope=openid&email&profile`
+  const url = `${data.authorization_endpoint}/auth?response_type=code&client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&scope=email`
 
   res.redirect(307, url).end()
 }
